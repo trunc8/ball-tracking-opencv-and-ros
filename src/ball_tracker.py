da53a54e18294a26086906b1fdaa50ecd5e36fc9
Siddharth Saha <sahasiddharth611@gmail.com>
2020-10-06 02:58:42 +0530

    Contours are now feature rich. There's an additional ball tracker against black background

--- src/ball_tracker.py
+++ src/ball_tracker.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 # trunc8 did this
+import numpy as np
 import cv2
 import rospy
 from cv_bridge import CvBridge, CvBridgeError
@@ -19,12 +20,14 @@ class Ball_Tracker:
       return
     mask_img = self.get_masked_image(cv_img)
     _, contours, hierarchy = cv2.findContours(mask_img, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    index = -1
-    thickness = 2
-    color = (255,0,255)
-    cv2.drawContours(cv_img, contours, index, color, thickness)
-    cv2.imshow("Ball Video", cv_img)
-    cv2.waitKey(3)
+    # One method of drawing contours
+    # index = -1
+    # thickness = 2
+    # color = (255,0,255)
+    # cv2.drawContours(cv_img, contours, index, color, thickness)
+    # cv2.imshow("Ball Video", cv_img)
+    # cv2.waitKey(3)
+    self.draw_ball_contours(cv_img, contours)
 
   def get_masked_image(self, rgb_img):
     hsv = cv2.cvtColor(rgb_img, cv2.COLOR_BGR2HSV)
@@ -33,6 +36,31 @@ class Ball_Tracker:
     yellowUpper = (60, 255, 255)
     mask = cv2.inRange(hsv, yellowLower, yellowUpper)
     return mask
+
+  def draw_ball_contours(self, rgb_img, contours):
+    black_img = np.zeros(rgb_img.shape, 'uint8')
+    for c in contours:
+      area = cv2.contourArea(c)
+      ((x,y), radius) = cv2.minEnclosingCircle(c)
+      if (area > 5000):
+        cv2.drawContours(rgb_img, [c], -1, (255,0,255), 2)
+        cx, cy = self.get_contour_center(c)
+        cv2.circle(rgb_img, (cx,cy), (int)(radius), (0,255,255), 3)
+        cv2.circle(black_img, (cx,cy), (int)(radius), (0,255,255), 3)
+        cv2.circle(black_img, (cx,cy), 5, (150,0,255), -1)
+      # print("Area: ", area)
+    cv2.imshow("Ball Tracking", rgb_img)
+    cv2.imshow("Black Background Tracking", black_img)
+    cv2.waitKey(3)
+
+  def get_contour_center(self, contour):
+    M = cv2.moments(contour)
+    cx = -1
+    cy = -1
+    if (M['m00'] != 0):
+      cx = int(M['m10']/M['m00'])
+      cy = int(M['m01']/M['m00'])
+    return cx, cy
 
 if __name__ == '__main__':
   try:
